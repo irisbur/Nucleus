e7e2333f38319eff76d5faaceecb2dbc8ad84e6f
Iris Burmistrov <iburmistrov@lightricks.com>
2023-01-05 09:29:16 +0200

    add some code to main..

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-
 import huffman
+import full_lz
+
+
+def deflate_compress(filename):
+    lengths_and_text, dis_vec = full_lz.lz_output(filename)
+    print("Items in first list: " + str(len(lengths_and_text)))
+    huff_lengths, tree = huffman.compress(lengths_and_text)
+    len(tree)
+
+    # todo: compress tree
+    encoded_tree = None
+
+    # todo: save as file
+    f = open(filename + "_compressed.bin", "wb")
+    # f.write()
+    return huff_lengths, encoded_tree
+
+
+def deflate_decompress(filename):
+    pass
 
 
 def main():
-    pass
+    deflate_compress("Samp1.bin")
+    # print("Decoded Output", huffman.decompress(huff_lengths, tree))
 
 
 if __name__ == '__main__':
