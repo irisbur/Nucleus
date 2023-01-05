4770276d1fad45d8dc221e51c1e5f4389496052f
ori <ori.meir@mail.huji.ac.il>
2023-01-05 10:32:32 +0200

    lz77

--- full_lz.py
+++ full_lz.py
@@ -4,6 +4,7 @@ from time import time
 def compress(text):
     encoded = []
     i = 0
+    dis_vec = []
     while i < len(text):
         best_len, best_index = 0, i
         for j in range(i - 1, -1, -1):
@@ -14,18 +15,20 @@ def compress(text):
                 best_len = k
                 best_index = j
         if best_len >= 4:
-            encoded += [[best_len, i - best_index]]
+            encoded.append(str(best_len))
+            dis_vec.append(i - best_index)
         else:
-            encoded += [text[i]]
+            encoded.append(text[i])
             best_len = 0
-        i += best_len + 1
-    return encoded
+        i += best_len if best_len else 1
+    return encoded, dis_vec
 
 
 def decompress(encoded, dis_vec):
     i = 0
     dis_index = 0
     decoded = []
+    added = 0
     for i in range(len(encoded)):
         if len(encoded[i]) == 8:
             decoded += [encoded[i]]
@@ -33,29 +36,24 @@ def decompress(encoded, dis_vec):
             length = int(encoded[i])
             dis = dis_vec[dis_index]
             for j in range(length):
-                decoded += [decoded[i - dis + j]]
+                decoded += [decoded[i + added - dis + j]]
+
+            added += length - 1
             dis_index += 1
-        i += 1
     return decoded
 
 def lz_output(file_name):
-    content = [f"{n:08b}" for n in open(file_name, "rb").read()][:120]
+    content = [f"{n:08b}" for n in open(file_name, "rb").read()]
     start = time()
-    encoded = compress(content)
+    encoded, dis_vec = compress(content)
     print(time() - start)
-    dis_vec = []
-    for i in range(len(encoded)):
-        if len(encoded[i]) != 8:
-            dis_vec.append(encoded[i][1])
-            encoded[i] = str(encoded[i][0])
-
-    print(content)
-    print(encoded, dis_vec)
+    # print(content)
+    # print(encoded, dis_vec)
     decoded = decompress(encoded, dis_vec)
-    print(decoded)
+    # print(decoded)
     print(decoded == content)
 
     return encoded, dis_vec
 
 
-lz_output("Samp1.bin")
+lz_output("Samp2    .bin")
