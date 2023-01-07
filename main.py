bb7aa59dd7b1da52a782326de11227bd22083f3b
Iris Burmistrov <iburmistrov@lightricks.com>
2023-01-07 10:53:23 +0200

    fix main and huffman

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import huffman
 import full_lz
 from bitarray import bitarray
 import compress_distances_vector
+import compress_tree
 
 DIST_BITS_NUM = 16
 HUFF_DICT_BITS_SIZE = 16
@@ -11,16 +12,19 @@ PADDING_BITS_NUM_SIZE = 8
 def deflate_compress(filename):
     lengths_and_text, dis_vec = full_lz.lz_output(filename)
     print("Items in first list: " + str(len(lengths_and_text)))
-    huff_lengths, huffman_encoding = huffman.compress(lengths_and_text)
+    huff_output, huffman_encoding = huffman.compress(lengths_and_text)
     encoded_dist = compress_distances_vector.compress_distances_vector_to_bits(dis_vec)
-    # todo: compress tree
-    encoded_huff_dict = None  # returns bits that represents the tree
 
-    all_compressed_data = encoded_dist + encoded_huff_dict + huff_lengths
-    num_zeros_to_pad = len(all_compressed_data) % 8
-    len_pad_byte = compress_distances_vector.pad(num_zeros_to_pad, 8)
+    encoded_huff_dict = compress_tree.compress_tree(huffman_encoding)  # returns bits that represents the tree
+
+    all_compressed_data = encoded_dist + encoded_huff_dict + huff_output
+    num_zeros_to_pad = (8 - len(all_compressed_data) % 8) * '0' if (8 - len(all_compressed_data) % 8) != 8 else ''
+    len_pad_byte = compress_distances_vector.pad(
+        compress_distances_vector.dem_to_bin(8-len(all_compressed_data) % 8), 8
+    )
     # save to binary file.
-    f = open(filename + "_compressed.bin", "wb")
+    f = open(filename[:-4] + "_compressed.bin", "wb")
+    print(len(len_pad_byte + num_zeros_to_pad + all_compressed_data))
     f.write(bitarray(len_pad_byte + num_zeros_to_pad + all_compressed_data))
     f.close()
 
@@ -35,35 +39,33 @@ def deflate_decompress(filename):
     bytes_data = bytes_data[PADDING_BITS_NUM_SIZE + pad_num:]
 
     # get distances vector for lz
-    dist_size = int(bytes_data[:DIST_BITS_NUM], 2)
-    encoded_dists = bytes_data[DIST_BITS_NUM: DIST_BITS_NUM + dist_size]
-    decoded_dists = compress_distances_vector.de_compress_distances_vector_from_bits(encoded_dists)
+    decoded_dists, bits_taken = compress_distances_vector.de_compress_distances_vector_from_bits(bytes_data)
 
     # crop out used data
-    bytes_data = bytes_data[DIST_BITS_NUM + dist_size:]
+    bytes_data = bytes_data[bits_taken:]
 
     # get huff code book
-    dict_size = int(bytes_data[:HUFF_DICT_BITS_SIZE])
-    encoded_huff_dict = bytes_data[HUFF_DICT_BITS_SIZE: HUFF_DICT_BITS_SIZE + dict_size]
-    decoded_huff_dict = None  # decode(encoded_huff_dict) # todo: decode dict :)
+    decoded_huff_dict, bits_taken = compress_tree.de_compress_tree(bytes_data)
     # crop out used data
-    bytes_data = bytes_data[HUFF_DICT_BITS_SIZE + dict_size:]
+    bytes_data = bytes_data[bits_taken:]
 
     # decode text & lengths using huffman
     decoded_huff_text = huffman.decompress(bytes_data, decoded_huff_dict)
 
     # decode to lz
     data = full_lz.decompress(decoded_huff_text, decoded_dists)
+    byte_data = [bytes(int(bits8, 2)) for bits8 in data]
     f = open(filename + "_decompressed.bin", 'wb')
-    f.write(data)
+    print("Im here!!111")
+    f.write(b''.join(byte_data))
     f.close()
 
 
 def main():
-    # f = open("Samp1.bin", "wb")
-    full_lz.lz_output("Samp1.bin")
-    # f.close()
-    # deflate_decompress("demofile.bin")
+    for i in range(1, 5):
+        print(f"running file {i} now")
+        deflate_compress(f"Samp{i}.bin")
+        deflate_decompress(f"Samp{i}_compressed.bin")
 
 
 if __name__ == '__main__':
